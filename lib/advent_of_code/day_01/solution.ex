c77a860429da5541f8676d6d6fce2672dd088721
gianniskr <giannis21kr@gmail.com>
2019-12-02 00:49:00 +0200

    completed first day

--- lib/advent_of_code/day_01/solution.ex
+++ lib/advent_of_code/day_01/solution.ex
@@ -2,13 +2,15 @@ defmodule AdventOfCode.Day01.Solution do
 
   def read() do
     list = AdventOfCode.Util.read("/lib/advent_of_code/day_01/puzzle_input.txt")
-    Enum.reduce(list, 0, fn x, acc -> calc(String.to_integer(x)) + acc end)
+    Enum.reduce(list, 0, fn x, acc -> calc(String.to_integer(x), 0) + acc end)
   end
 
+  def calc(mass, total_mass) when mass <= 0, do: total_mass
 
-
-  defp calc(mass) do
-     Integer.floor_div(mass, 3) - 2
+  def calc(mass, total_mass) do
+     res = Integer.floor_div(mass, 3) - 2
+     total_mass = if res > 0, do: total_mass + res, else: total_mass
+     calc(res, total_mass)
   end
 
 end
